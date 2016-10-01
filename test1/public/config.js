716d01fa17b11d5c29685a34618a9b6cf8a313b7
iwan <iwankruger@gmail.com>
2016-10-01 15:14:56 +0200

    Added a form to capture user information.  Also the application is displaying the users stored in the database

---
+++ test1/public/config.js
@@ -0,0 +1,4 @@
+'use strict';
+angular.module('app')
+    //.value('server_url','http://10.0.0.232:3000');
+    .value('server_url','http://localhost:3000');
--- test1/public/js/homeCtrl.js
+++ test1/public/js/homeCtrl.js
@@ -1,8 +1,24 @@
 angular
     .module('app')
-    .controller('homeCtrl', ['$scope','$http', function($scope,$http) {
-        console.log("Hello world from controller");
+    .controller('homeCtrl', ['$scope','$http','server_url', function($scope,$http,server_url) {
+        var server_address = server_url;
+        console.log("Hello world from controller server url = "+server_address);
         
+        var refresh = function() {
+            console.log("refresh items");
+            //console.log(getServerUrl());
+
+            $http.get(server_address+'/users').success(function(response){
+                console.log("I got the data that I requested");
+                console.log(response);
+                // store users in an object
+                $scope.user_list = response;
+                // clear input boxes
+                $scope.user = ""; 
+            });
+        };
+
+        refresh(); // get data when we load the page
 
   }]);
 
