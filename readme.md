ab3573172a0126b5b0a583c86482bfd1c849cb40
iwan <iwankruger@gmail.com>
2016-10-03 08:28:30 +0200

    Improved the generation of records.  The application also now checks for duplicate records

---
+++ readme.md
@@ -0,0 +1,34 @@
+The program requires Node.js, NPM and MongoDB
+
+Install Node.js and NPM (follow same instructions to update node and npm)
+1) Go to http://nodejs.org and download to latest LTS version (ie. download v4.5.0 LTS)
+2) Use installer to install node.js
+3) Check node and npm installation 
+In the terminal type the following: 
+#node -v
+v4.5.0
+#npm -v
+2.15.9
+
+Install Mongodb
+1) Go to https://www.mongodb.org then download and install MongoDB as per the instruction given there.
+2) Create a folder named mongodb on your computer and create a subfolder under it named data.
+3) Move to the mongodb folder and then start the MongoDB server by typing rhe following at the prompt:
+#mongod --dbpath=data
+
+In a terminal go to the test1 folder in this project and then
+
+Install project dependencies
+#npm install
+
+Start server
+#npm start
+
+In a browser go to
+http://localhost:3000/
+
+
+![alt tag](/user_program.png?raw=true)
+
+![alt tag](/csv_program.png?raw=true)
+
--- test1/public/js/csv_testCtrl.js
+++ test1/public/js/csv_testCtrl.js
@@ -51,25 +51,41 @@ angular
                         "Yellow", 
                         "Blue"
                         ];
+        // number of items to display on screen
+        $scope.limit_number_of_elements = 11;
+
+
+
+
+
 
 
 
         var refresh = function(){
             $scope.generate_success = false;
             $scope.form_record ={number:''};
+            $scope.limit_end = 11;
+            $scope.limit_begin = 0;
+            $scope.loading_file = false;
+            $scope.generate_records = false;
+            $scope.record_count = 0;
+
         }
 
         refresh();
         
-
         // generate a random date of birth
         var generate_date_of_birth = function(){
-            var start_date = new Date(1940,1,1);
+            var start_date = new Date(1920,1,1);
             //console.log("start date ",start_date);
             var end_date = new Date();
             //console.log("start date ",end_date);
             var random_date = new Date(start_date.getTime() + Math.random() * (end_date.getTime() - start_date.getTime()));
             //console.log("random date ",random_date);
+            //var random_year = Math.floor((Math.random() * 100) + 1915);
+            //var random_month = Math.floor((Math.random() * 11) + 0);
+            //var random_day = Math.floor((Math.random() * 31) + 1);
+            //var random_date = new Date(random_year,random_month,random_day);
             return random_date;
         }
 
@@ -81,108 +97,162 @@ angular
             return age;
         }
 
+
+        // check if there is a duplicate record in array
+        var duplicate_record = function(records_to_check,record){
+            for(var i=0;i<records_to_check.length;i++) {
+                if(records_to_check[i].Name == record.Name &&
+                   records_to_check[i].Surname == record.Surname &&
+                   records_to_check[i].DateOfBirth == record.DateOfBirth
+                   ) {
+                    //console.log("duplicate record position ",i);
+                    //console.log("record in db ",records_to_check[i]);
+                    //console.log("duplicate record ",record);
+                    // duplicate record 
+                    return true;
+                }
+            }
+            // not a duplicate record 
+            return false;
+
+        }
+
+        // generate records with random data for a csv file
         $scope.generate_csv_data = function(number_of_records_string){
-            var random_records = [];//[{a: 1, b:2}, {a:3, b:4}];
+            var random_records = [];
+            $scope.generate_records = true;
+            //$scope.$apply();
 
             // convert the sting to a integer
             var number_of_records_int = parseInt(number_of_records_string); 
             $scope.generate_success = false;
+            console.log("Generating csv file ...");
+
+            
+
+
 
             // generate random records
             for(var i=0;i<number_of_records_int;i++) {
-                var record = {Id:'', Name:'', Surname:'', Initials:'', Age:'', DateOfBirth:''};
-                // record id
-                record.Id = i;
-                // slect a random name
-                record.Name = $scope.names[Math.floor(Math.random() * $scope.names.length)];
-                // select a random surname
-                record.Surname = $scope.surnames[Math.floor(Math.random() * $scope.surnames.length)];
-                // get initials
-                var initials = record.Name.match(/\b\w/g);
-                record.Initials = initials.join('');
-                // generate a random birthday
-                var birthday = generate_date_of_birth();
-                record.DateOfBirth = $filter('date')(birthday, "dd/MM/yyyy");
-                // calculate age
-                record.Age = calculate_age(birthday);
-                random_records.push(record); 
-                //console.log(record);      
+            
+                (function (i) {
+                    setTimeout(function () {
+                        //console.log(i);
+                    
+                        do{
+                            var record = {Id:'', Name:'', Surname:'', Initials:'', Age:'', DateOfBirth:''};
+                            // record id
+                            record.Id = i;
+                            // slect a random name
+                            record.Name = $scope.names[Math.floor(Math.random() * $scope.names.length)];
+                            // select a random surname
+                            record.Surname = $scope.surnames[Math.floor(Math.random() * $scope.surnames.length)];
+                            // get initials
+                            var initials = record.Name.match(/\b\w/g);
+                            record.Initials = initials.join('');
+                            // generate a random birthday
+                            var birthday = generate_date_of_birth();
+                            record.DateOfBirth = $filter('date')(birthday, "dd/MM/yyyy");
+                            // calculate age
+                            record.Age = calculate_age(birthday);
+                            duplicate_record(random_records,record);
+                            //console.log(record);      
+                        }while (duplicate_record(random_records,record));
+
+                        
+                        
+                        // record not a duplicate add to array
+                        random_records.push(record); 
+                            
+                        // update view to indicate progress
+                        if(i-$scope.record_count > 1000) {
+                            $scope.record_count = i;
+                            $scope.$apply();
+                        }
+
+                           
+                        // upate view when finished
+                        if(i>=number_of_records_int-1) {
+                            $scope.array_to_write_to_csv = random_records;
+                            $scope.generate_success = true;
+                            $scope.generate_records = false;
+                            console.log("finished generating csv file");
+                            $scope.$apply();
+                        }
+                   
+                            return i;
+                    }, 0)
+                })(i)
+
+
+
+
+            }//for
+            
+            
+            
+
+            //duplicate_record(random_records,random_records[10]);
+
+
+        }
+
+
+        // upload csv file and store data in an array
+        $scope.uploadFile = function(){
+            var file = $scope.myFile;
+            
+            // indicate that the file is uploading
+            $scope.loading_file = true;
+
+            // Parse local CSV file
+            Papa.parse(file, {
+                complete: function(results) {
+                    //console.log("Finished:", results.data);
+                    //console.log("Finished:", results);
+                    $scope.file2 = results.data;
+                    console.log("Finished:");
+                    $scope.limit_begin = 0;
+                    $scope.loading_file = false;
+                    $scope.$apply();
+                }
+            });     
+        };
+
+        // load next items on page
+        $scope.load_next_data = function(){
+            if($scope.limit_begin + $scope.limit_number_of_elements  < $scope.file2.length) {
+                $scope.limit_begin = $scope.limit_begin + $scope.limit_number_of_elements;
+            } 
+        };
+
+        // load prevous items on page
+        $scope.load_prev_data = function(){
+
+            $scope.limit_begin = $scope.limit_begin - $scope.limit_number_of_elements;
+            if($scope.limit_begin < 0) {
+                $scope.limit_begin = 0;
             }
-            
-
-            $scope.array_to_write_to_csv = random_records;
-            $scope.generate_success = true;
-            console.log($scope.array_to_write_to_csv);
-        }
-
-        $scope.upload_file = function(){
-            console.log("file: ",$scope.fileContent);
-            //console.log("file length: ",$scope.fileContent.length);
-            console.log("mydata: ",$scope.myData);
-            console.log("file length: ",$scope.myData.data.length);
-
-            $scope.myData.data.splice(0, 1);
-            //$scope.myData.splice($scope.myData.length-1,1);
-            console.log("file length: ",$scope.myData.data.length);
-            console.log("file: ",$scope.myData.data[0]);
-            
-
-            
-            
-            
-        }
-
-        
-
-       
-
-
+             
+        };
         
 
   }])
-  .directive('fileReader', function() {
-  return {
-    scope: {
-      fileReader:"="
-    },
-    link: function(scope, element) {
-      $(element).on('change', function(changeEvent) {
-        var files = changeEvent.target.files;
-        if (files.length) {
-          var r = new FileReader();
-          r.onload = function(e) {
-              var contents = e.target.result;
-              scope.$apply(function () {
-                scope.fileReader = contents;
+  .directive('fileModel', ['$parse', function ($parse) {
+      return {
+          restrict: 'A',
+          link: function(scope, element, attrs) {
+              var model = $parse(attrs.fileModel);
+              var modelSetter = model.assign;
+            
+              element.bind('change', function(){
+                  scope.$apply(function(){
+                      modelSetter(scope, element[0].files[0]);
+                  });
               });
-          };
-          
-          r.readAsText(files[0]);
-        }
-      });
-    }
-  };
-})
-.directive('csvUpload', function () {
-  return {
-    restrict: 'E',
-    template: '<input type="file" onchange="angular.element(this).scope().handleFiles(this.files)">',
-    require: 'ngModel',
-    scope: {},
-    link: function (scope, element, attrs, ngModel) {
-      scope.handleFiles = function (files) {
-        Papa.parse(files[0], {
-          dynamicTyping: true,
-          complete: function(results) {
-            // you can transform the uploaded data here if necessary
-            // ...
-            ngModel.$setViewValue(results);
           }
-        });
       };
+}]);
 
-    }
-  };
-});
   
 
